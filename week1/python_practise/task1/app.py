014f0da87642d0519cc3abdf944d65786da19626
KnollElias <knoll.elias@icloud.com>
2024-10-26 11:12:44 +0200

    Rename practise

---
+++ week1/python_practise/task1/app.py
@@ -0,0 +1,8 @@
+name = "Elias"
+alter = 20
+grösse = 1.75
+
+def print_greeting(name, alter, grösse):
+  print(f"Hallo, ich heisse {name}, bin {alter} Jahre alt und {grösse} Meter gross.")
+
+print_greeting(name, alter, grösse)
