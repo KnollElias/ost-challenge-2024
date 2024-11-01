1bc3bdcfdcd56fbb7934037ff9c12371ed206c2f
KnollElias <107244530+KnollElias@users.noreply.github.com>
2024-11-01 17:57:52 +0100

    Add tasks

---
+++ week1/Aufgabe 1.1/app.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from typing import List
+
+def get_image_paths(path: str) -> List[Path]:
+    image_paths = sorted(Path(path).resolve().rglob("*.jpg"), key=lambda x: x.name)
+    return image_paths
+
+# Tests
+# Diese Test könnt ihr benützten, um euren Code zu prüfen. Bitte jedoch nicht ändern
+
+# Arrange
+import tempfile
+p = Path(tempfile.gettempdir()) / "test1-1"; p.mkdir(exist_ok=True)
+test_pats = [p / f"{i}.jpg" for i in range(5)]
+[p.touch() for p in reversed(test_pats)];
+negative = p / "negative.txt"; negative.touch()
+
+# Assert
+assert type(get_image_paths(p)) == list, "Die Funktion sollte eine Liste zurückgeben."
+assert len(get_image_paths(p)) == len(test_pats), "Die Funktion sollte im Test genau 5 JPG-Bilder finden"
+assert all(p.is_absolute() for p in get_image_paths('.')), "Die Funktion sollte absolute Pfade zurückgeben"
+assert all(a==b for a,b in zip(get_image_paths(p), test_pats)), "Die Funktion sollte die korrekten Pfade und sortiert zurückgeben"
+
+# Cleanup
+import shutil; shutil.rmtree(p)
---
+++ week1/Aufgabe 1.3/app.py
@@ -0,0 +1,32 @@
+from PIL import Image
+from pathlib import Path
+
+def open_image(path: Path, resize=None) -> Image:
+    img = Image.open(path)
+    if resize:
+        img = img.resize(resize)
+    return img
+
+# Tests
+
+# Arrange
+import tempfile
+p = Path(tempfile.gettempdir()) / "test1-3"; p.mkdir(exist_ok=True)
+img_path = p / "test.jpg"
+img = Image.new('RGB', (20, 20), 'white')
+img.save(img_path)
+
+# Assert
+assert issubclass(type(open_image(img_path)), Image.Image) , "Die Funktion sollte ein Image-Objekt zurückgeben"
+assert open_image(img_path).size == (20, 20), "Das Bild sollte nicht verändert werden wenn kein resize angegeben ist"
+assert open_image(img_path, (10, 10)).size == (10, 10), "Das Bild sollte verkleinert werden wenn ein resize angegeben ist"
+
+# Cleanup
+import shutil; shutil.rmtree(p)
+
+# Dev test
+# img_path = Path(r"C:\Users\KNOLLLI\Pictures\Screenshots\test.png")
+# image = open_image(img_path)
+# print(f"Image size without resizing: {image.size}")
+# resized_image = open_image(img_path, resize=(100, 100))
+# print(f"Image size with resizing: {resized_image.size}")
