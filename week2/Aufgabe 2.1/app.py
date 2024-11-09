f49a106bed546218608d96b0b02b77e592e7a77d
KnollElias <knoll.elias@icloud.com>
2024-11-10 00:14:17 +0100

    Finish task 1

--- week2/Aufgabe 2.1/app.py
+++ week2/Aufgabe 2.1/app.py
@@ -1,6 +1,10 @@
 import lightning as L
 import torch
 
-print(f"Is Cuda available: {torch.cuda.is_available()}")
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-print(torch.cuda.get_device_name(device=device))
+def get_label(file_path: str) -> int:
+    return int(file_path.split('_')[-1].split('.')[0])
+
+test_path = "02_Homework/mnist_1k/mnist0_2.png"
+assert get_label(test_path) == 2, "Fehler bei der Extraktion des Labels (2)"
+test_path = "02_Homework/mnist_1k/mnist999_0.png"
+assert get_label(test_path) == 0, "Fehler bei der Extraktion des Labels (0)"
